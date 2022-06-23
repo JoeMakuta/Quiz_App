405ba3b8cdaeaf83585f569651fbd50f22ccc49a
Josue Makuta <makutajosue@gmail.com>
2022-06-23 02:50:13 +0200

    Login Validation okay

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-let myButton = document.querySelector(".startButton")
+let myStartButton = document.querySelector("#startButton")
 let firstInput = document.getElementById("firstInput")
 let secondInput = document.getElementById("secondInput")
 let login = document.querySelector(".login")
@@ -11,11 +11,15 @@ let homePageB = document.getElementById("homePageB")
 let userName = document.querySelector("#userName")
 let userEmail = document.querySelector("#userEmail")
 let quitButton = document.querySelector(".quitButton")
-
+let span1 = document.querySelector(".firstInput>span")
+let span2 = document.querySelector(".secondInput>span")
 
 let timerWidth = 100;
 let startTime = 59;
 let coutDown = document.getElementById("coutDown")
+
+const print = console.log.bind(globalThis);
+
 function counter() {
    if (startTime >= 0) {
       coutDown.textContent = `${startTime}`
@@ -26,25 +30,10 @@ function counter() {
    }
 }
 
-myButton.addEventListener('click', function () {
-
-   if (firstInput.value == "" || secondInput.value == "") {
-      if (firstInput.value == "") {
-         let span1 = document.querySelector(".firstInput>span")
-         span1.textContent = "N’oubliez pas de renseigner votre nom avant de commencer le Quiz. ."
-         span1.style.color = "red"
-         span1.style.fontSize = "10px"
-         document.querySelector("#firstInput").style.border = " 1px solid rgba(255, 56, 56, 1)"
-      } else if (secondInput.value == "") {
-         let span2 = document.querySelector(".secondInput>span")
-         span2.textContent = "N’oubliez pas de renseigner votre email avant de commencer le Quiz"
-         span2.style.color = "red"
-         span2.style.fontSize = "10px"
-         document.querySelector("#secondInput").style.border = " 1px solid rgba(255, 56, 56, 1)"
-      }
-
-   }
-   else {
+function clickCommencer() {
+   const regex1 = /^([a-z A-Z]{4,50})$/;
+   const regex2 = /^([a-zA-Z\._\-0-9]{4,50})@([a-zA-Z0-9]{3,10})\.([a-zA-Z]{2,5})$/;
+   if (regex1.test(firstInput.value) && regex2.test(secondInput.value)) {
       login.style.display = "none"
       questions.style.display = "block"
       resultContainer.style.display = "none"
@@ -52,7 +41,51 @@ myButton.addEventListener('click', function () {
       userEmail.textContent = secondInput.value
       setInterval(counter, 1000)
    }
-});
+}
+
+function firstInputListen() {
+   print("The input works ...")
+   let regex = /^([a-z A-Z]{4,50})$/;
+   if (!regex.test(firstInput.value)) {
+      firstInput.style.color = "red";
+      span1.textContent = "N’oubliez pas de renseigner votre nom avant de commencer le Quiz. ."
+      span1.style.color = "red"
+      span1.style.fontSize = "10px"
+      firstInput.style.border = " 1px solid rgba(255, 56, 56, 1)"
+   } else {
+      firstInput.style.color = "black";
+      span1.textContent = ""
+      firstInput.style.border = " 1px solid black";
+
+   }
+}
+
+function secondInputListen() {
+   print("The input works ...")
+   let regex = /^([a-zA-Z\._\-0-9]{4,50})@([a-zA-Z0-9]{3,10})\.([a-zA-Z]{2,5})$/;
+   if (!regex.test(secondInput.value)) {
+      secondInput.style.color = "red";
+      span2.textContent = "N’oubliez pas de renseigner votre email avant de commencer le Quiz"
+      span2.style.color = "red"
+      span2.style.fontSize = "10px"
+      secondInput.style.border = " 1px solid rgba(255, 56, 56, 1)"
+
+   } else {
+      secondInput.style.color = "black";
+      span2.textContent = ""
+      secondInput.style.border = " 1px solid black";
+   }
+}
+
+
+firstInput.addEventListener("input", firstInputListen);
+
+secondInput.addEventListener("input", secondInputListen);
+
+
+myStartButton.addEventListener('click', clickCommencer);
+
+
 
 homePageB.addEventListener("click", function () {
    window.open("index.html", "_top")
@@ -62,5 +95,5 @@ quitButton.addEventListener("click", function () {
    login.style.display = "none"
    questions.style.display = "none"
    resultContainer.style.display = "block"
-   
+
 });
